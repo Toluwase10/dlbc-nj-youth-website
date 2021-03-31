afa1f6664e92564b075e3b5e35cbaf4893955580
Emmanuel <etejuosho@dlbcnj.org>
2021-03-31 14:29:54 -0400

    added node server and ejs files

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 
-//setup date for footer
-let dateYear = document.getElementById("date");
+//Express server
+const app = express();
+const port = process.env.PORT || 3000;
 
-dateYear.innerHTML = new Date().getFullYear();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+/**
+ * -------------- ROUTES ----------------
+ */
+
+ //get home page
+app.get("/", function (req, res){
+    res.render("home");
+  });
+
+
+
+
+
+
+
+
+
+//Connect to server
+app.listen(port, function(){
+  console.log("Server is listening on port " + port);
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
 // ********** nav toggle ************
 // select button and links
-const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
+// const navBtn = document.getElementById("nav-toggle");
+// const links = document.getElementById("nav-links");
 // add event listener
-navBtn.addEventListener("click", () => {
-  links.classList.toggle("show-links");
-});
+// navBtn.addEventListener("click", () => {
+//   links.classList.toggle("show-links");
+// });
 
 
